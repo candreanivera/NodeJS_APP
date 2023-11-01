181c5f9c5e73773d4d8a16ac2be0cbcb0073efaa
Cristina Andreani <candreanivera@gmail.com>
2023-10-31 23:45:56 -0700

    Changes in comments

--- app/controllers/items.js
+++ app/controllers/items.js
@@ -1,16 +1,7 @@
-<<<<<<< HEAD
-/*Definition of the function getData for Items
-  It will be imported on the routes files */
+/*Definition of the function getData for Items. It will be imported on the routes files */
 
 const data = {};
 
 exports.getData = (req, res) => {
     res.send({ data: 'Information coming from Items'});
 }
-=======
-/**Function getData is exported, it must be imported inside the Routes files */
-
-exports.getData = (req, res) => {
-    res.send({ data: 'Esto viene desde ITEMS'});
-}
->>>>>>> 900b934844dc00e253cf8d82c1837d2aa1124a69
--- app/controllers/upload.js
+++ app/controllers/upload.js
@@ -1,13 +1,8 @@
 /**U P L O A D   F I L E S */
 const multer = require('multer');
 
-<<<<<<< HEAD
 /**Define how the files will be named
  * Files will be uploaded to the upload folder
-=======
-/** filename: how the file is going to be named after uploaded. Dates are used to avoid duplicity.
- * files will be stored inside 'uploads' folder
->>>>>>> 900b934844dc00e253cf8d82c1837d2aa1124a69
  */
 const storage = multer.diskStorage({
     destination : (req, file, cb) => {
@@ -21,7 +16,3 @@ const storage = multer.diskStorage({
 const upload = multer({storage: storage});
 
 module.exports = upload;
-<<<<<<< HEAD
-
-=======
->>>>>>> 900b934844dc00e253cf8d82c1837d2aa1124a69
