f5234551a3092e33a8a460d7398ab99a830f76fe
Cristina Andreani <candreanivera@gmail.com>
2023-11-01 06:48:58 -0700

    Changes in the comments

--- app/controllers/upload.js
+++ app/controllers/upload.js
@@ -1,8 +1,7 @@
 /**U P L O A D   F I L E S */
 const multer = require('multer');
 
-/**Define how the files will be named
- * Files will be uploaded to the upload folder
+/**Define how the files will be named. Files will be uploaded to the upload folder
  */
 const storage = multer.diskStorage({
     destination : (req, file, cb) => {
